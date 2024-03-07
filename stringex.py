fd8f2be2c08decae2b0d5058f087931d5e638731
Yangdema <03230157.gcbs@rub.edu.bt>
2024-03-07 15:11:13 +0600

    string task

---
+++ stringex.py
@@ -0,0 +1,16 @@
+#get user input
+# convert it into ALL upper case
+# print the convert string 
+#A -4
+#o - 0
+#e - 3
+#l - 1
+#s - 5
+#g - 9
+#b - 6
+#t - 7
+userInput = input('enter a string:')
+userInputConverted = userInput.upper()
+print(userInputConverted)
+
+
