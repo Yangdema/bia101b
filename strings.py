386f8608071d611e08d64850a2483e91becd9477
Yangdema <03230157.gcbs@rub.edu.bt>
2024-03-07 15:01:58 +0600

    string manupolation

---
+++ strings.py
@@ -0,0 +1,31 @@
+#string manupulation 
+#string concatenation
+string1 = "hello "
+string2 = "world"
+combined = string1 + string2
+combined1 = "hi" + " " + "there"
+print('string concatenation')
+print(combined)
+print(combined1)
+
+#string interpolation
+print('string interpolation')
+name = "John"
+age = 30
+interpolation = f"John is {age} years of age"
+print(interpolation)
+
+#string methods
+print('string methods')
+#upper
+print('string upper')
+stringlower = "hello how are you"
+converted = stringlower.upper()
+converted1 = "pure STRING".upper()
+print(converted)
+print(converted1)
+#capitalize
+print('string capitalize')
+capitalize = "hello there". capitalize()
+print(capitalize)
+
