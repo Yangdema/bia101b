c426febe3cdfe91ab4286831a36fa2f2a851257c
Yangdema <03230157.gcbs@rub.edu.bt>
2024-03-28 14:16:18 +0600

    bubblesort

---
+++ unit4/bubblesort.py
@@ -0,0 +1,19 @@
+input_arr = [6,3,1,5,0]
+def bubble_sort(arr):
+    n = len(arr)
+    print('before first for loop')
+    for i in range(n): # 0,1,2,3,4,5
+        print('inside first for loop')
+        print()
+        for k in range(0,(n-1)):
+            element = arr[k]
+            elementright = arr[k+1]
+            print('elementright:' , element)
+            print('element:' , elementright)
+            print('==================')
+            #swap
+            if element > elementright:
+                arr[k] = elementright
+                arr[k+1] = element
+bubble_sort(input_arr)
+
---
+++ unit4/linear.py
@@ -0,0 +1,36 @@
+#searching 
+#sorting
+#problem 1
+#input
+user_input = [1,2,3,4,5,6,7,8,9,11]
+#10: check to see if a certain number exist in the user input array
+n = 14
+
+#linear search
+result = False 
+for e in user_input:
+    if e == n:
+        result = True
+        
+    if result == True:
+        print('found')
+    else:
+        print('not found')
+
+#if else, for loops,print, calculations (+,-)
+#time: 0(n)
+input = [1,2,1]
+for i in input:  #n^3 + n^2  n^3
+    print('hi')
+    for m in input: #n^2
+        print('hello')
+        for l in input: #n
+            print('hello')
+for k in input:
+    print('hello')
+        
+
+
+   
+        
+    
